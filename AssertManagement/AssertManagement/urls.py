2e4dfaa2f8652dad0c59ccff1119662ecb46c430
msamido <tsmapurunyane@gmail.com>
2018-03-14 15:42:58 +0200

    Url.py added

--- AssertManagement/AssertManagement/urls.py
+++ AssertManagement/AssertManagement/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns = [
+    path('Asserts/', include('Asserts.urls')),
     path('admin/', admin.site.urls),
 ]
